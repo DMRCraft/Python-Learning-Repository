219d388490bd4046cf30c3165b187c90dd0921e7
Dylan Ryan <dylandevitt2002@gmail.com>
2026-09-12 21:10:14 +0100

    13 - Data Structures

---
+++ 13 - Data Structures/structures.md
@@ -0,0 +1,10 @@
+# Collections (will add to as I discover note-worthy points)
+
+## What is the point of tuples, if you can't change or add to them?
+
+Simply, they are used for a grouped set of values that are guarenteed to not be changed <br>
+E.g: cords = (10, 20) <br>
+Another example: location = {
+(10, 20): "Home",
+(100, -20): "Village"
+}
---
+++ 13 - Data Structures/structures.py
@@ -0,0 +1,65 @@
+# Collection (data structure) is a data type used to store and organise multiple values as a single object / "variable"
+
+# List []  | ordered, mutable, changeable collection, and duplicates are OK
+# Set {}   | unordered and mutable adding and removing are fine. NO duplicates
+# Tuple () | ordered and unchangeable. Duplicates are OK
+
+# For a list of methods available to a certain collection, use:
+# print(dir(collection)) - alphabetical list of methods
+# print(help(collection)) - documentation for the object
+
+
+fruits = ["apple", "orange", "banana", "strawberry"]
+
+print(fruits[0])
+print(fruits[0:3]) # these work as string indexing
+print(fruits[::2])
+
+# for item in collection
+for fruit in fruits:
+    print(fruit)
+
+print(len(fruits))
+print("apple" in fruits) # the "in" operator returns a boolean, True if the collection contains it, else False
+
+# ----- LISTS -----
+
+fruits[1] = "pineapple"
+
+fruits.append("coconut")    # append to the end of the list
+fruits.remove("apple")      # remove the first occurance of "apple"
+fruits.insert(0, "grape")   # insert "grape" at index 0
+fruits.sort()               # sort into ALPHABETICAL ORDEWR
+fruits.reverse()            # reverse the list
+
+print(fruits.index("apple"))  # returns the index of the first occurance. Throws a ValueError if isn't found
+print(fruits.count("coconut"))
+
+fruits.clear()              # remove all elements
+
+print(fruits)
+
+# -------------------------
+
+# ----- SETS -----
+
+vegetables = {"broccoli", "carrot", "potato"}
+print(vegetables) # restart the program multiple times and see the order change
+
+print(len(vegetables))
+print("brocoli" in vegetables)
+
+vegetables.add("lettuce")
+vegetables.pop() # removes an arbitrary element
+vegetables.clear()
+
+print(vegetables)
+
+# -------------------------
+
+# ----- Tuples -----
+
+colors = ("red", "orange", "yellow")
+print(len(colors))
+print("red" in colors)
+print(colors.count("red"))
