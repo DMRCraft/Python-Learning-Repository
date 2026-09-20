5ed1c2732ce0c9a21fb76acb48b2f93ad3946d0c
Dylan Ryan <dylandevitt2002@gmail.com>
2026-09-20 16:34:14 +0100

    OOP 8 - Aggregation

---
+++ 25 - OOP/OOP 8 - Aggregation/main.py
@@ -0,0 +1,36 @@
+# Aggregation is an OOP relationship where one object   (the whole)
+#             contains references to one or more INDEPENDENT objects (the parts)
+#             think of it as a "has-a" relationship
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        return [f"{book.title} by {book.author}" for book in self.books]
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+library = Library("Central Library")
+
+book1 = Book("Harry Potter", "J.K Rowling")
+book2 = Book("Lord of the Rings", "J. R. R. Tolkien")
+book3 = Book("The Hunger Games", "Suzanne Collins")
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+print(library.name)
+
+for num, book in enumerate(library.list_books(), start=1):
+    print(f"{num}. {book}")
